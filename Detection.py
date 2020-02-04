d305ee98da4d2ddc6b8ca581c5672f59e7d4546e
Lechner, Richard <lechnerri@mediamarktsaturn.com>
2020-02-04 14:30:47 +0100

    Notizen hinzugefügt

--- Detection.py
+++ Detection.py
@@ -4,7 +4,7 @@
 # importing OpenCV, time and Pandas library
 import cv2, time, pandas
 # importing datetime class from datetime library
-from datetime import datetime 
+from datetime import datetime
 
 # Assigning our static_back to None
 static_back = None
